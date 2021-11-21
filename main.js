d2026d0e1dfc03815451820455f3894fa2797945
Bauski <baku.kim@gmail.com>
2021-11-21 11:46:59 -0800

    Created display module and completed fretboard display.

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 // Import all necessary classes
 // FretStore for all Fret states
 import { makeFretStore } from './modules/fretStore.js';
+// Display stuff
+import { mount, displayFretboard, displayMenu } from './modules/display.js';
 
 // Fretboard creator
 const fretStore = makeFretStore();
-
-console.log(fretStore);
+let fretboardDisplay = displayFretboard(fretStore);
+let menuDisplay = displayMenu(fretStore);
+mount('#fretboard-viewport', fretboardDisplay);
+//mount('#menuViewport',menuDisplay);
