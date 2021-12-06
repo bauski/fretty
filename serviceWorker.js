8f80d0b585e170042dff14b2127c77e14833355d
Bauski <baku.kim@gmail.com>
2021-12-05 17:01:54 -0800

    Fixed service worker. Removed dynamic request caching.

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,31 @@
 const cacheName = 'fretty.cache.0.02';
+var urlsToCache = [
+    '/',
+    '/modules/colors.js',
+    '/modules/display.js',
+    '/modules/eventHandler.js',
+    '/modules/fret.js',
+    '/modules/fretStore.js',
+    '/modules/music.js',
+    '/modules/observer.js',
+    '/index.html',
+    '/main.js',
+    '/assets/fretboard.css'
+];
+
+self.addEventListener('install', event => {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
 
 self.addEventListener('activate', event => {
+    console.log('sw activated');
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
@@ -13,19 +38,13 @@ self.addEventListener('activate', event => {
         })
     );
 });
+
 self.addEventListener('fetch', event => {
-    if(!event.request.url.startsWith('http')){
-        event.respondWith(
-            fetch(event.request)
-                .then(response => {
-                    const appClone = response.clone();
-                    caches
-                        .open(cacheName)
-                        .then(cache => {
-                            cache.put(event.request, appClone);
-                        });
-                    return response;
-                })
-        );
-    }
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                return response || fetch(event.request);
+            })
+    );
 });
